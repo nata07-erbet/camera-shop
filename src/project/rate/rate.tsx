150bee6e0c4a1a2e1615833ce1dc8b79836bd5ea
Nata <erbet@mail.ru>
2024-07-10 21:48:08 +0300

    add rate

---
+++ src/project/rate/rate.tsx
@@ -0,0 +1,27 @@
+function Rate() {
+  return (
+    <div className="rate product__rate">
+      <svg width={17} height={16} aria-hidden="true">
+        <use xlinkHref="#icon-full-star" />
+      </svg>
+      <svg width={17} height={16} aria-hidden="true">
+        <use xlinkHref="#icon-full-star" />
+      </svg>
+      <svg width={17} height={16} aria-hidden="true">
+        <use xlinkHref="#icon-full-star" />
+      </svg>
+      <svg width={17} height={16} aria-hidden="true">
+        <use xlinkHref="#icon-full-star" />
+      </svg>
+      <svg width={17} height={16} aria-hidden="true">
+        <use xlinkHref="#icon-star" />
+      </svg>
+      <p className="visually-hidden">Рейтинг: 4</p>
+      <p className="rate__count">
+        <span className="visually-hidden">Всего оценок:</span>12
+      </p>
+    </div>
+  )
+};
+
+export { Rate };
